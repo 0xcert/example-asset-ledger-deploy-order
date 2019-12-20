0fcc42ce5ed57b3f9c468d701ea48416a35a9ead
Tadej Vengust <vengust.tadej@gmail.com>
2019-12-20 15:18:47 +0100

    Asset ledger deploy order logic.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ This is an example of how to use a Metamask provider in 0xcert framework v2.
 
 Project stucture:
 
-| Path | Description
-|-|-
-| src/example.ts | Main logic showing the use.
-| index.html | Front end styling.
-| index.ts | Controller connecting front end to the logic.
-| package.json | Dependencies.
+| Path           | Description                                   |
+| -------------- | --------------------------------------------- |
+| src/example.ts | Main logic showing the use.                   |
+| index.html     | Front end styling.                            |
+| index.ts       | Controller connecting front end to the logic. |
+| package.json   | Dependencies.                                 |
--- index.ts
+++ index.ts
@@ -1,13 +1,98 @@
-const console = document.getElementById("console");
+import { config } from "./src/config";
+import {
+  checkApprovedValue,
+  approveValue,
+  signOrder,
+  provider,
+  performOrder
+} from "./src/example";
 
-function printMessage(message: any) {
-  if (typeof message !== "string") {
-    message = JSON.stringify(message, null, 2);
+const divConsole = document.getElementById("console");
+const btnApproveTransfer = document.getElementById("btnApproveTransfer");
+const btnSignOrder = document.getElementById("btnSignOrder");
+const btnPerformOrder = document.getElementById("btnPerformOrder");
+
+btnApproveTransfer.addEventListener("click", async () => {
+  if (config.valueLedgerId === "") {
+    printWarning(
+      "No valueLedgerId defined. Set value ledger id in src/config.ts file."
+    );
+    return;
   }
-  const div = document.createElement("div");
-  div.innerText = message;
-  console.prepend(div);
-}
+
+  if (config.account1Id === "") {
+    printWarning("No account1Id defined. Please set it in src/config.ts file.");
+    return;
+  }
+
+  const isApproved = await checkApprovedValue().catch(e => {
+    printError(e);
+  });
+
+  if (isApproved) {
+    printMessage("Value already approved.");
+  } else {
+    const mutation = await approveValue().catch(e => {
+      printError(e);
+    });
+    if (mutation) {
+      printMessage("Value approval in progress: " + mutation.id);
+      printMessage("This may take a while.");
+      await mutation.complete();
+      printMessage("Value approval completed.");
+    }
+  }
+});
+
+btnSignOrder.addEventListener("click", async () => {
+  if (config.valueLedgerId === "") {
+    printWarning(
+      "No valueLedgerId defined. Set value ledger id in src/config.ts file."
+    );
+    return;
+  }
+
+  if (config.account1Id === "") {
+    printWarning("No account1Id defined. Please set it in src/config.ts file.");
+    return;
+  }
+
+  if (provider.accountId !== config.account1Id) {
+    printWarning("Select account1 in metamask to sign this order.");
+    return;
+  }
+
+  let error = null;
+  await signOrder().catch(e => {
+    error = e;
+    printError(e);
+  });
+
+  if (!error) {
+    printMessage("Order signing sucessfull: " + config.signature);
+  }
+});
+
+btnPerformOrder.addEventListener("click", async () => {
+  if (config.signature === "") {
+    printWarning("No signature provided. Please sign the order first.");
+    return;
+  }
+
+  const mutation = await performOrder().catch(e => {
+    printError(e);
+  });
+
+  if (mutation) {
+    printMessage("Atomic deployment in progress: " + mutation.id);
+    printMessage("This may take a while.");
+    await mutation.complete();
+    printMessage("Atomic deployment completed");
+    printMessage(
+      "Created asset ledger address: " + mutation.logs[0].createdContract
+    );
+  }
+});
 
 function printError(message: any) {
   if (typeof message !== "string") {
@@ -16,5 +101,24 @@ function printError(message: any) {
   const div = document.createElement("div");
   div.innerText = "Error: " + message;
   div.className = "error";
-  console.prepend(div);
+  divConsole.prepend(div);
+}
+
+function printWarning(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = "Warning: " + message;
+  div.className = "warning";
+  divConsole.prepend(div);
+}
+
+function printMessage(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = message;
+  divConsole.prepend(div);
 }
